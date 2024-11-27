5cdf06383ed2a7a206e0efe4ebe6628e713ccd76
TFMB <mail@vanilla-station.ru>
2024-11-27 12:35:12 +0300

    upstream fix

--- Content.Shared/Vanilla/CCVars/CCVarsVanilla.cs
+++ Content.Shared/Vanilla/CCVars/CCVarsVanilla.cs
@@ -10,8 +10,7 @@ public sealed class CCVarsVanilla
     /// <summary>
     /// URL вебхука, используемого для отправки сообщений о банах на Discord сервер.
     /// </summary>
-    public static readonly CVarDef<string> DiscordServerBansWebhook =
-        CVarDef.Create("discord.server_bans_webhook", "", CVar.SERVERONLY);
+    public static readonly CVarDef<string> DiscordServerBansWebhook = CVarDef.Create("discord.server_bans_webhook", "", CVar.SERVERONLY);
 
 
     /*
@@ -20,8 +19,7 @@ public sealed class CCVarsVanilla
     /// <summary>
     ///     Controls if the connections queue is enabled. If enabled stop kicking new players after `SoftMaxPlayers` cap and instead add them to queue.
     /// </summary>
-    public static readonly CVarDef<bool>
-        QueueEnabled = CVarDef.Create("queue.enabled", false, CVar.SERVERONLY);
+    public static readonly CVarDef<bool> QueueEnabled = CVarDef.Create("queue.enabled", false, CVar.SERVERONLY);
      /*
      * Discord Auth
      */
@@ -29,25 +27,20 @@ public sealed class CCVarsVanilla
     /// <summary>
     ///     Enabled Discord linking, show linking button and modal window
     /// </summary>
-    public static readonly CVarDef<bool> DiscordAuthEnabled =
-        CVarDef.Create("discord_auth.enabled", false, CVar.SERVERONLY);
+    public static readonly CVarDef<bool> DiscordAuthEnabled = CVarDef.Create("discord_auth.enabled", false, CVar.SERVERONLY);
 
     /// <summary>
     ///     URL of the Discord auth server API
     /// </summary>
-    public static readonly CVarDef<string> DiscordAuthApiUrl =
-        CVarDef.Create("discord_auth.api_url", "", CVar.SERVERONLY);
+    public static readonly CVarDef<string> DiscordAuthApiUrl = CVarDef.Create("discord_auth.api_url", "", CVar.SERVERONLY);
 
     /// <summary>
     ///     Secret key of the Discord auth server API
     /// </summary>
-    public static readonly CVarDef<string> DiscordAuthApiKey =
-        CVarDef.Create("discord_auth.api_key", "", CVar.SERVERONLY | CVar.CONFIDENTIAL);    
-        CVarDef.Create("discord.server_bans_webhook", string.Empty, CVar.SERVERONLY);
+    public static readonly CVarDef<string> DiscordAuthApiKey = CVarDef.Create("discord_auth.api_key", "", CVar.SERVERONLY | CVar.CONFIDENTIAL);    
         
     /// <summary>
     /// задержка между вызовами обр
     /// </summary>
-    public static readonly CVarDef<int> SpecForceDelay =
-        CVarDef.Create("specforce.delay", 2, CVar.SERVERONLY);
+    public static readonly CVarDef<int> SpecForceDelay = CVarDef.Create("specforce.delay", 2, CVar.SERVERONLY);
 }
