072fe7853fffc7ec1603518c06bb8a697aa934b6
TFMB <mail@vanilla-station.ru>
2025-07-15 23:57:41 +0300

    anticheat improve

--- Content.Client/Vanilla/Anticheat/ClientAnticheatManager.cs
+++ Content.Client/Vanilla/Anticheat/ClientAnticheatManager.cs
@@ -3,6 +3,8 @@ using Content.Shared.Ghost;
 using Content.Shared.Weapons.Ranged.Events;
 using Content.Shared.Interaction;
 using Content.Shared.Revenant.Components;
+using Content.Shared.Examine;
+using Content.Shared.Movement.Components;
 using Robust.Shared.GameObjects;
 using Robust.Shared.Log;
 using Robust.Shared.Network;
@@ -19,18 +21,8 @@ namespace Content.Client.Vanilla.Anticheat
     public sealed class ClientAnticheatManager : EntitySystem
     {
         [Dependency] private readonly IGameTiming _timing = default!;
-        [Dependency] private readonly IOverlayManager _overlayManager = default!;
-        [Dependency] private readonly INetManager _netManager = default!;
-
-        [Dependency] private readonly IEntityManager _entMan = default!;
         [Dependency] private readonly IPlayerManager _playerManager = default!;
-        [Dependency] private readonly IEyeManager _eyeManager = default!;
-        [Dependency] private readonly ILightManager _light = default!;
-        [Dependency] private readonly SharedInteractionSystem _interactionSystem = default!;
-        /// <summary>
-        /// При наличии 5+ читпоинтов отправляется репорт
-        /// </summary>
-        private int cheatpoints = 0;
+        [Dependency] private readonly ExamineSystemShared _examine = default!;
         private TimeSpan _nextCheck = TimeSpan.Zero;
 
         public override void Initialize()
@@ -48,34 +40,13 @@ namespace Content.Client.Vanilla.Anticheat
                 return;
 
             // Перебираем все ловушки
-            foreach (var trap in EntityQuery<AnticheatTrapComponent>())
+            var query = EntityQueryEnumerator<AnticheatTrapComponent>();
+            while (query.MoveNext(out var trapUid, out _))
             {
-                var trapUid = trap.Owner;
-
-                // Игрок не должен видеть ловушку
-                if (!IsEntityVisibleToPlayer(trapUid, playerEnt))
-                    cheatpoints += 5;
+                //игрок не должен видеть ловушку
+                if (_examine.InRangeUnOccluded(playerEnt, trapUid, 10f, ignoreInsideBlocker: false))
+                    ReportCheat($"Увидел крысиную ловушку");
             }
-        }
-
-        private bool IsEntityVisibleToPlayer(EntityUid target, EntityUid player)
-        {
-            // Проверяем, что оба валидны
-            if (!_entMan.EntityExists(target) || !_entMan.EntityExists(player))
-                return false;
-
-            if (!TryComp<TransformComponent>(target, out var targetXform))
-                return false;
-
-            if (!TryComp<TransformComponent>(player, out var playerXform))
-                return false;
-
-            // Проверяем, что они на одной карте
-            if (targetXform.MapID != playerXform.MapID)
-                return false;
-
-            // Проверяем преграды
-            return _interactionSystem.InRangeUnobstructed(player, target, range: 16.0f);
         }
 
         private void OnAimBotBait(RequestShootEvent msg, EntitySessionEventArgs args)
@@ -84,64 +55,47 @@ namespace Content.Client.Vanilla.Anticheat
         }
 
 
-        // public override void Update(float frameTime)
-        // {
-        //     base.Update(frameTime);
-
-        //     var now = _timing.CurTime;
-
-        //     if (now < _nextCheck)
-        //         return;
-
-        //     if (cheatpoints >= 5)
-        //     {
-        //         ReportCheat($"Рейтинг подозрения: {cheatpoints}");
-        //         return;
-        //     }
-
-        //     _nextCheck = now + TimeSpan.FromSeconds(5);
-        //     CheckForVisibleTraps();
-        //     // CheckDrawFovFlag();
-        // }
-
-        private void CheckDrawFovFlag()
+        public override void Update(float frameTime)
         {
-            // Получаем сессию локального игрока
-            var session = _playerManager.LocalSession;
-            if (session == null)
+            base.Update(frameTime);
+
+            var now = _timing.CurTime;
+
+            if (now < _nextCheck)
                 return;
 
-            var playerEnt = session.AttachedEntity;
-            if (playerEnt == null)
-                return;
-
-            // Игрок не должен быть призраком
-            if (HasComp<GhostComponent>(playerEnt.Value))
-                return;
-            if (HasComp<RevenantComponent>(playerEnt.Value))
-                return;
-
-            if (!_eyeManager.CurrentEye.DrawFov)
-            {
-                cheatpoints += 1;
-            }
-
-            if (!_light.Enabled)
-            {
-                cheatpoints += 1;
-            }
-
-            if (!_light.DrawShadows)
-            {
-                cheatpoints += 1;
-            }
+            _nextCheck = now + TimeSpan.FromSeconds(5);
+            CheckForVisibleTraps();
+            CheckDrawFovFlag();
         }
+
+    private void CheckDrawFovFlag()
+    {
+        var session = _playerManager.LocalSession;
+        if (session == null)
+            return;
+
+        var playerEnt = session.AttachedEntity;
+        if (playerEnt == null)
+            return;
+
+        if (HasComp<GhostComponent>(playerEnt.Value) || HasComp<RevenantComponent>(playerEnt.Value))
+            return;
+
+        if (TryComp<EyeComponent>(playerEnt.Value, out var eye) && !eye.NetSyncEnabled)
+        {
+            ReportCheat("Отключена синхронизация EyeComponent");
+        }
+        if (TryComp<ContentEyeComponent>(playerEnt.Value, out var contentEye) && !contentEye.NetSyncEnabled)
+        {
+            ReportCheat("Отключена синхронизация ContentEyeComponent");
+        }
+    }
 
         private void ReportCheat(string reason)
         {
             var msg = new SuspiciousClientEvent(reason);
             RaiseNetworkEvent(msg);
-            cheatpoints = 0;
         }
     }
 }
