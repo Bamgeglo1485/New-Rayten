a2e757e406afe36384f02ec299f2a164273acd0e
TFMB <mail@vanilla-station.ru>
2025-09-23 23:40:40 +0300

    10skillpoints

--- Content.Shared/Vanilla/RoleSkills/SharedRoleSkillsSystem.cs
+++ Content.Shared/Vanilla/RoleSkills/SharedRoleSkillsSystem.cs
@@ -2,7 +2,7 @@ namespace Content.Shared.Vanilla.RoleSkills;
 
 public sealed class SharedRoleSkillsSystem : EntitySystem
 {
-    public const int skillpoints = 9;
+    public const int skillpoints = 10;
     public static string GetJobPrototype(string? roleSkills)
     {
         if (string.IsNullOrEmpty(roleSkills))
