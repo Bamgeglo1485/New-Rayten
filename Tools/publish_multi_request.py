0a83d920f952454f085acb2134df808f67ad7d3e
Lemniscate <103616394+thefuckmybrain@users.noreply.github.com>
2026-06-08 09:37:00 +0300

    Update publish_multi_request.py

--- Tools/publish_multi_request.py
+++ Tools/publish_multi_request.py
@@ -15,7 +15,7 @@ RELEASE_DIR = "release"
 # CONFIGURATION PARAMETERS
 # Forks should change these to publish to their own infrastructure.
 #
-ROBUST_CDN_URL = "http://45.10.43.42:27690/"
+ROBUST_CDN_URL = "http://186.246.10.223:27690/"
 FORK_ID = "Main"
 
 def main():
