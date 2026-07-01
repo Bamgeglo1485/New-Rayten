a91f192b89f9ba72ed98184310b254ea9faca4a3
Bamgeglo1485 <akkauntmin@gmail.com>
2026-07-01 18:06:56 +0900

    adasdasd

--- Content.Shared/Atmos/EntitySystems/SharedAtmosphereSystem.Gases.cs
+++ Content.Shared/Atmos/EntitySystems/SharedAtmosphereSystem.Gases.cs
@@ -9,8 +9,6 @@ namespace Content.Shared.Atmos.EntitySystems;
 
 public abstract partial class SharedAtmosphereSystem
 {
-    [Dependency] private readonly IPrototypeManager ProtoMan = default!;
-
     /*
      Partial class for operations involving GasMixtures.
 
