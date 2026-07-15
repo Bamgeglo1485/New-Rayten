5930ada35797b803699ea57ca268e37e33382750
Bamgeglo1485 <akkauntmin@gmail.com>
2026-07-16 04:42:45 +0900

    Sec loadouts

--- Content.Goobstation.UIKit/Assembly.cs
+++
@@ -1,6 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
-using Avalonia.Metadata;
-
-[assembly: XmlnsDefinition("https://goobstation.com/", clrNamespace: "Content.GoobStation.UIKit.Widgets")]
-[assembly: XmlnsDefinition("https://goobstation.com/", clrNamespace: "Content.GoobStation.UIKit.Widgets.Smart")]
--- Content.Goobstation.UIKit/Entry/EntryPoint.cs
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-
-using Robust.Shared.ContentPack;
-
-namespace Content.Goobstation.UIKit.Entry;
-
-public sealed class EntryPoint : GameClient;
