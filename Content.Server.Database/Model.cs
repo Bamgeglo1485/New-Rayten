c5cdbe78c6e3038ee9be6dd14849112672f471aa
Bamgeglo1485 <akkauntmin@gmail.com>
2026-08-20 00:06:35 +0900

    adafd

--- Content.Server.Database/Model.cs
+++ Content.Server.Database/Model.cs
@@ -355,8 +355,8 @@ namespace Content.Server.Database
         public string? Voice { get; set; } = null!; // If null, the voice gets defaulted to the sex associated value
         public string Gender { get; set; } = null!;
         public string Species { get; set; } = null!;
-        public string BarkVoice { get; set; } = "Papyrus"; // Rayten-TTS
-        public float BarkVoicePitch { get; set; } = 1.0f; // Rayten-TTS
+        [Column("bark_voice")] public string BarkVoice { get; set; } = "Papyrus"; // Rayten-TTS
+        [Column("bark_voice_pitch")] public float BarkVoicePitch { get; set; } = 1.0f; // Rayten-TTS
         [Column(TypeName = "jsonb")] public JsonDocument? OrganMarkings { get; set; } = null!;
         [Column(TypeName = "jsonb")] public JsonDocument? Markings { get; set; } = null!;
         public string HairName { get; set; } = null!;
